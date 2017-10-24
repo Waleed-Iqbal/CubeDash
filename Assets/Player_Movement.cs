4fd0267237e78b356c11d36f3bac25ae2f3e7b74
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-10-24 22:02:23 +0500

    Unity Development started
    First checkin.

---
+++ Assets/Player_Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Marked as "Fixed"Update because we are using it to mess with physics
+    void Update()
+    {
+        rb.AddForce(0, 0, 1000 * Time.deltaTime);
+
+    }
+}
