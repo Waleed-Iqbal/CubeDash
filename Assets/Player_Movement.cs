f54b32a855e449308f47d4ebed288a20359afced
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-10-25 23:51:10 +0500

    Some movements

--- Assets/Player_Movement.cs
+++ Assets/Player_Movement.cs
@@ -4,7 +4,9 @@ public class Player_Movement : MonoBehaviour
 {
 
     public Rigidbody rb;
-
+    public float forwardForce = 500f;
+    public float rightSideForce = 50f;
+    public float leftSideForce = -50f;
     // Use this for initialization
     void Start()
     {
@@ -12,9 +14,20 @@ public class Player_Movement : MonoBehaviour
     }
 
     // Marked as "Fixed"Update because we are using it to mess with physics
-    void FixedUpdate()
+    void Update()
     {
-        rb.AddForce(0, 0, 1000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(rightSideForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(leftSideForce * Time.deltaTime, 0, 0);
+        }
+
 
     }
 }
