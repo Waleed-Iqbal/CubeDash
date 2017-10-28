06740b65a1d1d10e82f253136162444c0e17067f
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-10-28 18:38:38 +0500

    Obstacle asset added

---
+++ Assets/Player_Collision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Collision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+
+        if (collisionInfo.collider.tag ==  "Obstacle")
+        {
+            Debug.Log("Obstacle hit");
+        }
+    }
+}
