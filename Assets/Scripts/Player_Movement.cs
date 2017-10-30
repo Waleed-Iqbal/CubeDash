04f6101c21a40b3904f99a096a3d08d8f2f6dadd
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-10-30 21:50:14 +0500

    Folders and some text added

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float sideWayForce = 120f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Marked as "Fixed"Update because we are using it to mess with physics
+    void Update()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+            rb.AddForce(sideWayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        if (Input.GetKey("a"))
+            rb.AddForce(-sideWayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+
+    }
+}
