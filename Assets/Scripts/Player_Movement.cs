4980407825aa5cb15f5e5a515435ebed609f0f1b
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-10-31 23:58:31 +0500

    Forward and backward force added

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -6,6 +6,8 @@ public class Player_Movement : MonoBehaviour
     public Rigidbody rb;
     public float forwardForce = 1000f;
     public float sideWayForce = 120f;
+    public float backwardForce = 1000f;
+
     // Use this for initialization
     void Start()
     {
@@ -15,13 +17,16 @@ public class Player_Movement : MonoBehaviour
     // Marked as "Fixed"Update because we are using it to mess with physics
     void Update()
     {
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("w"))
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
         if (Input.GetKey("d"))
             rb.AddForce(sideWayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-
         if (Input.GetKey("a"))
             rb.AddForce(-sideWayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        if (Input.GetKey("s"))
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
 
 
     }
