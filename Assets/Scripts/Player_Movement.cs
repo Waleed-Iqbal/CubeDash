4c63090a36e4171f80821542a6baf4d79726d425
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-11-08 00:28:00 +0500

    jumps and horizontal movements improved.

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -5,15 +5,12 @@ public class Player_Movement : MonoBehaviour
 
     public Rigidbody rb;
     public float forwardForce = 1000f;
-    public float sideWayForce = 120f;
-    public float backwardForce = 1000f;
-    private float maxRightXPosition = 4f;
+    private float maxRightXPosition = 3.4f;
     private float middleXPosition = 1f;
-    private float maxLeftXPosition = -2f;
+    private float maxLeftXPosition = -1.4f;
 
-    private float xPositionDifference = 1;
+    private float xPositionDifference = 1.2f;
 
-    // x -3.743 ----- x 5.752
     // Use this for initialization
     void Start()
     {
@@ -25,27 +22,31 @@ public class Player_Movement : MonoBehaviour
     {
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        if (Input.GetKey("w"))
+        if (Input.GetKeyDown(KeyCode.W) && rb.position.y <= 0.76f)  // only jump when on ground
+            rb.AddForce(0, forwardForce * 10 * Time.deltaTime, 0);
+
+        if (rb.position.y > 2f)
+            rb.AddForce(0, -forwardForce/2 * Time.deltaTime, 0);
+
+        if (Input.GetKeyDown(KeyCode.D)) //Input.GetKey("d"))
         {
-            if (rb.position.y <= 0.76f) // only jump when on ground
-                rb.AddForce(0, forwardForce * 7 * Time.deltaTime, 0);
 
+            if (rb.position.x == middleXPosition)
+                rb.MovePosition(new Vector3(maxRightXPosition, rb.position.y, rb.position.z));
+            else if (rb.position.x == maxLeftXPosition)
+                rb.MovePosition(new Vector3(middleXPosition, rb.position.y, rb.position.z));
         }
-
-        if (Input.GetKey("d"))
-            if (rb.position.x < maxRightXPosition)
-                rb.MovePosition(new Vector3(rb.position.x + xPositionDifference, rb.position.y, rb.position.z));
 
         //    rb.AddForce(sideWayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 
-        if (Input.GetKey("a"))
-            if (rb.position.x > maxLeftXPosition)
-                rb.MovePosition(new Vector3(rb.position.x - xPositionDifference, rb.position.y, rb.position.z));
+        if (Input.GetKeyDown(KeyCode.A))
+        {
 
-        //AddForce(-sideWayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-
-        //if (Input.GetKey("s"))
-        //    rb.AddForce(0, 0, -50 * Time.deltaTime, ForceMode.VelocityChange);
+            if (rb.position.x == maxRightXPosition)
+                rb.MovePosition(new Vector3(middleXPosition, rb.position.y, rb.position.z));
+            else if (rb.position.x == middleXPosition)
+                rb.MovePosition(new Vector3(maxLeftXPosition, rb.position.y, rb.position.z));
+        }
 
 
     }
