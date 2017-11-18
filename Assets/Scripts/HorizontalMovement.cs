25ab6aa491ee7ee4776c0c199b2ccba7ebec2505
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-11-18 19:25:56 +0500

    Some minor UI changes to look better on mobile

--- Assets/Scripts/HorizontalMovement.cs
+++ Assets/Scripts/HorizontalMovement.cs
@@ -4,9 +4,16 @@ public class HorizontalMovement : MonoBehaviour
 {
 
     // Use this for initialization
-    private bool movingTowardLeft = true;
-    private float offsetEachFrame = 0f;
-    private float magnitude = 4f;
+    private bool movingTowardLeft;
+    private float offsetEachFrame;
+    private float magnitude;
+
+    private void Start()
+    {
+        movingTowardLeft = true;
+        magnitude = 4f;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -14,9 +21,11 @@ public class HorizontalMovement : MonoBehaviour
         transform.position = new Vector3(transform.position.x + offsetEachFrame, transform.position.y, transform.position.z);
 
         if (transform.position.x <= -magnitude)
-            movingTowardLeft = false;
+            movingTowardLeft = false; // now start moving to right
         else if (transform.position.x >= magnitude)
-            movingTowardLeft = true;
+            movingTowardLeft = true; // now start moving to left
+
+
 
     }
 }
