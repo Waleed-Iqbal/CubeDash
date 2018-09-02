2e47cee5a31b87ec9462856fa9bfafbd73a03a63
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2018-09-02 16:21:29 +0500

    Updated level(s) design

--- Assets/Scripts/HomeScene.cs
+++ Assets/Scripts/HomeScene.cs
@@ -3,12 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class HomeScene : MonoBehaviour
 {
-        //public LevelComplete levelComplete;// = new LevelComplete();
     public void LetsBeatItClicked()
     {
-        //levelComplete.LoadNextLevel();
-
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
 }
